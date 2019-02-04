f141fc4850ea384e697e58bf90576a52c67bd768
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2019-02-04 23:56:48 +0300

    update example up to the latest testNG version fixes reportportal/example-java-TestNG#11

--- logback/src/main/java/com/epam/rp/tests/extension/ParameterizedTest.java
+++ logback/src/main/java/com/epam/rp/tests/extension/ParameterizedTest.java
@@ -1,11 +1,6 @@
 package com.epam.rp.tests.extension;
 
-import com.epam.reportportal.listeners.ListenerParameters;
-import com.epam.reportportal.service.ReportPortalClient;
 import com.epam.reportportal.testng.BaseTestNGListener;
-import com.epam.reportportal.testng.ITestNGService;
-import com.epam.reportportal.testng.TestNGAgentModule;
-import com.epam.reportportal.testng.TestNGProvider;
 import com.epam.reportportal.testng.TestNGService;
 import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
 import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
@@ -18,15 +13,12 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import rp.com.google.common.base.Optional;
 import rp.com.google.common.base.Throwables;
-import rp.com.google.inject.Module;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
-import static rp.com.google.inject.util.Modules.override;
 
 /**
  * Extension example
@@ -36,74 +28,60 @@ import static rp.com.google.inject.util.Modules.override;
 @Listeners({ ParameterizedTest.ExtendedListener.class })
 public class ParameterizedTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ParameterizedTest.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ParameterizedTest.class);
 
-    @Test(threadPoolSize = 2, dataProvider = "bla-bla")
-    public void testParams(String msg) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            LOGGER.info(msg + ": " + i);
-            if (i == 1) {
-                Thread.sleep(TimeUnit.SECONDS.toMillis(5L));
-            }
-        }
-    }
+	@Test(threadPoolSize = 2, dataProvider = "bla-bla")
+	public void testParams(String msg) throws InterruptedException {
+		for (int i = 0; i < 10; i++) {
+			LOGGER.info(msg + ": " + i);
+			if (i == 1) {
+				Thread.sleep(TimeUnit.SECONDS.toMillis(5L));
+			}
+		}
+	}
 
-    @Test
-    public void demoFailure() throws InterruptedException {
-        Assert.fail("Horrible exception in description");
-    }
+	@Test
+	public void demoFailure() throws InterruptedException {
+		Assert.fail("Horrible exception in description");
+	}
 
-    @DataProvider(parallel = true, name = "bla-bla")
-    public Iterator<Object[]> params() {
-        return Arrays.asList(new Object[] { "one" }, new Object[] { "two" }).iterator();
-    }
+	@DataProvider(parallel = true, name = "bla-bla")
+	public Iterator<Object[]> params() {
+		return Arrays.asList(new Object[] { "one" }, new Object[] { "two" }).iterator();
+	}
 
-    public static class ExtendedListener extends BaseTestNGListener {
-        public ExtendedListener() {
-            super(override(new TestNGAgentModule()).with((Module) binder -> binder.bind(ITestNGService.class)
-                    .toProvider(new TestNGProvider() {
-                        @Override
-                        protected TestNGService createTestNgService(ListenerParameters listenerParameters,
-                                ReportPortalClient reportPortalClient) {
-                            return new ParamTaggingTestNgService(listenerParameters, reportPortalClient);
-                        }
-                    })));
-        }
-    }
+	public static class ExtendedListener extends BaseTestNGListener {
+		public ExtendedListener() {
+			super(new ParamTaggingTestNgService());
+		}
+	}
 
-    public static class ParamTaggingTestNgService extends TestNGService {
+	public static class ParamTaggingTestNgService extends TestNGService {
 
-        public ParamTaggingTestNgService(ListenerParameters parameters, ReportPortalClient reportPortalClient) {
-            super(parameters, reportPortalClient);
-        }
+		@Override
+		protected StartTestItemRQ buildStartStepRq(ITestResult testResult) {
+			final StartTestItemRQ rq = super.buildStartStepRq(testResult);
+			if (testResult.getParameters() != null && testResult.getParameters().length != 0) {
+				final Set<String> tags = Optional.fromNullable(rq.getTags()).or(new HashSet<>());
+				for (Object param : testResult.getParameters()) {
+					tags.add(param.toString());
+				}
+				rq.setTags(tags);
 
-        @Override
-        protected StartTestItemRQ buildStartStepRq(ITestResult testResult) {
-            final StartTestItemRQ rq = super.buildStartStepRq(testResult);
-            if (testResult.getParameters() != null && testResult.getParameters().length != 0) {
-                final Set<String> tags = Optional.fromNullable(rq.getTags()).or(new HashSet<>());
-                for (Object param : testResult.getParameters()) {
-                    tags.add(param.toString());
-                }
-                rq.setTags(tags);
+			}
+			return rq;
+		}
 
-            }
-            return rq;
-        }
-
-        @Override
-        protected FinishTestItemRQ buildFinishTestMethodRq(String status, ITestResult testResult) {
-            FinishTestItemRQ finishTestItemRQ = super.buildFinishTestMethodRq(status, testResult);
-            if (testResult.getThrowable() != null) {
-                String description =
-                        "```error\n"
-                                + Throwables.getStackTraceAsString(testResult.getThrowable())
-                                + "\n```";
-                description = description + Throwables.getStackTraceAsString(testResult.getThrowable());
-                finishTestItemRQ.setDescription(description);
-            }
-            return finishTestItemRQ;
-        }
-    }
+		@Override
+		protected FinishTestItemRQ buildFinishTestMethodRq(String status, ITestResult testResult) {
+			FinishTestItemRQ finishTestItemRQ = super.buildFinishTestMethodRq(status, testResult);
+			if (testResult.getThrowable() != null) {
+				String description = "```error\n" + Throwables.getStackTraceAsString(testResult.getThrowable()) + "\n```";
+				description = description + Throwables.getStackTraceAsString(testResult.getThrowable());
+				finishTestItemRQ.setDescription(description);
+			}
+			return finishTestItemRQ;
+		}
+	}
 
 }
